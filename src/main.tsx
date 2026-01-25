a0271373d5722a75d5f5895f0c7b59cbcceb6d67
Sandesh Gadal <gadalsandesh123@gmail.com>
2026-01-25 23:51:16 +0545

    add the google tag manager scripts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './app/App.tsx'
-import './styles/index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { HelmetProvider, Helmet } from 'react-helmet-async';
+import App from './app/App.tsx';
+import './styles/index.css';
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+        <Helmet>
+
+        </Helmet>
+        
+        <App />
+    </HelmetProvider>
   </StrictMode>,
-)
+);
